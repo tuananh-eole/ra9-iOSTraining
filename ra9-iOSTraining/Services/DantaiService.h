07d8070c5b5df28ecc5fafc7642ac6b2d826694c
tuananh <t-anh@eole.co.jp>
2016-09-30 19:49:03 +0900

    入らないファイル削除

---
+++ ra9-iOSTraining/Services/DantaiService.h
@@ -0,0 +1,13 @@
+//
+//  DantaiService.h
+//  ra9-iOSTraining
+//
+//  Created by トゥアンアイン on 2016/09/30.
+//  Copyright © 2016年 トゥアンアイン. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DantaiService : NSObject
+
+@end
---
+++ ra9-iOSTraining/Services/DantaiService.m
@@ -0,0 +1,13 @@
+//
+//  DantaiService.m
+//  ra9-iOSTraining
+//
+//  Created by トゥアンアイン on 2016/09/30.
+//  Copyright © 2016年 トゥアンアイン. All rights reserved.
+//
+
+#import "DantaiService.h"
+
+@implementation DantaiService
+
+@end
